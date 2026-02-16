6b030ac4daccb25edabb9717133fcfef2fd607ab
Shirley Thatcher <shirleywetindi15@gmail.com>
2026-02-17 00:06:44 +0300

    chore(config): update backend settings configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://todo-backend-9c70.onrender.com",
     "https://todo-frontend-p5nf.onrender.com",
 ]
 
